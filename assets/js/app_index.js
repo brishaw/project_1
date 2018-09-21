08e822278ebb21af639b3b98f14e8f154046945b
Ethan <ethan.holston@gmail.com>
2018-09-20 20:01:17 -0400

    allowed searching on city, state

--- assets/js/app_index.js
+++ assets/js/app_index.js
@@ -10,14 +10,41 @@ var config = {
 firebase.initializeApp(config);
 
 var dataRef = firebase.database();
+var zip;
+var isZip = new RegExp(/\d{5}/);
+
 
 $(".zip-search").on("click", function (event) {
     event.preventDefault();
-    var zip = $(".zip-input").val();
-    localStorage.clear();
-    localStorage.setItem("zip", zip);
-    dataRef.ref().push({
-        zip: zip
-    });
-    window.location.href = "results.html";
+    zip = $(".zip-input").val().trim();
+    if(isZip.test(zip) != true){
+        zip = zip.split(",");
+        var city = zip[0].trim();
+        var state = zip[1].trim();
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/D2TblXN7Wgz5Ik9j5mgTBJPWR2VVLC3ZCc2wr8t5e53ktlUi3sI39ZZt7O096rGu/city-zips.json/" + city + "/" + state,
+            method: "GET"
+        }).then(function(response) {
+            if(response.zip_codes.length % 2 == 0){
+            zip = response.zip_codes[(response.zip_codes.length / 2)];
+            } else 
+            {
+                zip = response.zip_codes[((response.zip_codes.length -1) / 2)]
+            }
+            localStorage.clear();
+            localStorage.setItem("zip", zip);
+            dataRef.ref().push({
+                zip: zip
+            });
+            window.location.href = "results.html";
+        });
+    } 
+    else {
+        localStorage.clear();
+        localStorage.setItem("zip", zip);
+        dataRef.ref().push({
+            zip: zip
+        });
+        window.location.href = "results.html";
+    }
 });
